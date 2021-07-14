eeb68729e812b08d827758c129a273b61fd38c58
Michael Meckler <meckler@dslextreme.com>
2021-07-13 17:26:59 -0700

    Fixed autoloaded constant deprecation warning

--- lib/authorio/engine.rb
+++ lib/authorio/engine.rb
@@ -1,14 +1,15 @@
 module Authorio
-  class Engine < ::Rails::Engine
+	class Engine < ::Rails::Engine
 		isolate_namespace Authorio
 
 		initializer "authorio.load_helpers" do |app|
-			ActionController::Base.send :include, Authorio::Helpers
+			Rails.application.reloader.to_prepare do
+				ActionController::Base.send :include, Authorio::Helpers
+			end
 		end
 
 		initializer "authorio.assets.precompile" do |app|
 			app.config.assets.precompile += %w( authorio/auth.css )
 		end
-
-  end
+	end
 end
